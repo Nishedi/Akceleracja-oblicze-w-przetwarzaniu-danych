295341de0f86beef80c736dc65c161c202912141
Nishedi <263948@student.pwr.edu.pl>
2024-10-29 14:00:56 +0100

    Poprawione menu

--- main.py
+++ main.py
@@ -5,43 +5,51 @@ from utils import load_numbers_from_file, stopwatch
 
 
 def main():
-    print("Choose strategy:")
-    print("1. CPU")
+    while True:
+        print("Choose strategy:")
+        print("1. CPU")
+        print("2. Parrael CPU")
 
-    choice = input("Enter the number of your choice: ")
+        choice = input("Enter the number of your choice: ")
 
-    if choice == '1':
-        strategy = CPUPrimalityTestStrategy()
-    if choice == '2':
-        strategy = CPUParrarelPrimalityStrategyStrategy()
-    else:
-        print("Invalid choice, defaulting to CPU strategy.")
-        strategy = CPUPrimalityTestStrategy()
+        if choice == '1':
+            strategy = CPUPrimalityTestStrategy()
+        if choice == '2':
+            strategy = CPUParrarelPrimalityStrategyStrategy()
+        else:
+            print("Invalid choice, defaulting to CPU strategy.")
+            strategy = CPUPrimalityTestStrategy()
 
-    miller_rabin_test = MillerRabinTest(strategy)
+        miller_rabin_test = MillerRabinTest(strategy)
 
-    print("\nTest numbers:")
-    print("1. Enter number manually")
-    print("2. Load numbers from a file")
-    input_mode = input("Enter the number of your choice: ")
+        print("\nTest numbers:")
+        print("1. Enter number manually")
+        print("2. Load numbers from a file")
+        print("3. Load default value for test purposes (4754597)")
+        input_mode = input("Enter the number of your choice: ")
 
-    if input_mode == '1':
-        n = int(input("Enter the number to check: "))
-        k = int(input("Enter the number of iterations (default 10): ") or 10)
-        is_prime = stopwatch(miller_rabin_test.is_prime)(n, k)
-        print(f"Number {n} is prime: {is_prime}")
-    elif input_mode == '2':
-        file_path = input("Enter the file path: ") or 'numbers.txt'
-        try:
-            numbers = load_numbers_from_file(file_path)
+        if input_mode == '1':
+            n = int(input("Enter the number to check: "))
             k = int(input("Enter the number of iterations (default 10): ") or 10)
-            for number in numbers:
-                is_prime = stopwatch(miller_rabin_test.is_prime)(number, k)
-                print(f"Number {number} is prime: {is_prime}")
-        except FileNotFoundError:
-            print(f"File {file_path} not found.")
-    else:
-        print("Invalid input mode selected.")
+            is_prime = stopwatch(miller_rabin_test.is_prime)(n, k)
+            print(f"Number {n} is prime: {is_prime}")
+        elif input_mode == '2':
+            file_path = input("Enter the file path: ") or 'numbers.txt'
+            try:
+                numbers = load_numbers_from_file(file_path)
+                k = int(input("Enter the number of iterations (default 10): ") or 10)
+                for number in numbers:
+                    is_prime = stopwatch(miller_rabin_test.is_prime)(number, k)
+                    print(f"Number {number} is prime: {is_prime}")
+            except FileNotFoundError:
+                print(f"File {file_path} not found.")
+        elif input_mode == '3':
+            n = 4754597
+            k = 100000000
+            is_prime = stopwatch(miller_rabin_test.is_prime)(n, k)
+            print(f"Number {n} is prime: {is_prime}")
+        else:
+            print("Invalid input mode selected.")
 
 
 if __name__ == '__main__':
