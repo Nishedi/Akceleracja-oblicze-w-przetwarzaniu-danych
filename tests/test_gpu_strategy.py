a4cfc0073b9cb3b6d90503ab11a4755d24ef39c4
Ernest K <ernio789@gmail.com>
2024-11-23 12:38:01 +0100

    add tests for GPU version

---
+++ tests/test_gpu_strategy.py
@@ -0,0 +1,51 @@
+import unittest
+from miller_rabin import MillerRabinTest
+from random_base_generator import RandomBaseGenerator
+from strategies.gpu_strategy import GPUPrimalityTestStrategy
+from utils import load_numbers_from_file
+
+
+class TestGPUPrimalityTest(unittest.TestCase):
+    def setUp(self):
+        self.gpu_strategy = GPUPrimalityTestStrategy()
+        self.miller_rabin_test = MillerRabinTest(self.gpu_strategy)
+
+    def test_prime(self):
+        k = 100
+        random_bases = RandomBaseGenerator.generate_bases(31, k)
+        self.assertTrue(self.miller_rabin_test.is_prime(31, k, random_bases))
+        random_bases = RandomBaseGenerator.generate_bases(35, k)
+        self.assertFalse(self.miller_rabin_test.is_prime(35, k, random_bases))
+
+    def test_non_primes(self):
+        self._test_from_file("data/non_primes.csv", False)
+
+    def test_primes_20_bits(self):
+        self._test_from_file("data/primes_20_bits.csv", True)
+
+    def test_primes_43_bits(self):
+        self._test_from_file("data/primes_43_bits.csv", True)
+
+    def test_primes_63_bits(self):
+        self._test_from_file("data/primes_63_bits.csv", True)
+
+    def test_edge_cases_non_primes(self):
+        self._test_from_file("data/edge_cases_false.csv", False)
+
+    def test_edge_cases_primes(self):
+        self._test_from_file("data/edge_cases_true.csv", True)
+
+    def test_pseudo_primes(self):
+        self._test_from_file("data/pseudo_primes.csv", False)
+
+    def _test_from_file(self, file_path, expected: bool):
+        numbers = load_numbers_from_file(file_path)
+        k = 50
+        for number in numbers:
+            random_bases = RandomBaseGenerator.generate_bases(number, k)
+            result = self.miller_rabin_test.is_prime(number, k, random_bases)
+            self.assertEqual(result, expected, f"Failed on {number} (expected {expected})")
+
+
+if __name__ == '__main__':
+    unittest.main()
